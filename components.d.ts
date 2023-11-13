da565831aec653876c90364e0586b55eb7afa7bb
skwhuber <149645549+skwhuber@users.noreply.github.com>
2023-11-13 15:41:18 +0800

    Update components.d.ts
    //update by huber begin bak

--- components.d.ts
+++ components.d.ts
@@ -35,3 +35,36 @@ declare module '@vue/runtime-core' {
     TheHeader: typeof import('./src/components/header/TheHeader.vue')['default']
   }
 }
+
+//update by huber begin bak
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    IconClose: typeof import('./src/components/icons/IconClose.vue')['default']
+    IconDisconnect: typeof import('./src/components/icons/IconDisconnect.vue')['default']
+    IconKey: typeof import('./src/components/icons/IconKey.vue')['default']
+    IconKeystore: typeof import('./src/components/icons/IconKeystore.vue')['default']
+    IconLedger: typeof import('./src/components/icons/IconLedger.vue')['default']
+    IconLeft: typeof import('./src/components/icons/IconLeft.vue')['default']
+    IconMenu: typeof import('./src/components/icons/IconMenu.vue')['default']
+    IconMnemonic: typeof import('./src/components/icons/IconMnemonic.vue')['default']
+    LangSwitch: typeof import('./src/components/header/LangSwitch.vue')['default']
+    MenuBar: typeof import('./src/components/header/MenuBar.vue')['default']
+    NAlert: typeof import('naive-ui')['NAlert']
+    NAvatar: typeof import('naive-ui')['NAvatar']
+    NButton: typeof import('naive-ui')['NButton']
+    NDropdown: typeof import('naive-ui')['NDropdown']
+    NIcon: typeof import('naive-ui')['NIcon']
+    NMessageProvider: typeof import('naive-ui')['NMessageProvider']
+    NNotificationProvider: typeof import('naive-ui')['NNotificationProvider']
+    NSpace: typeof import('naive-ui')['NSpace']
+    NSpin: typeof import('naive-ui')['NSpin']
+    NTag: typeof import('naive-ui')['NTag']
+    NUpload: typeof import('naive-ui')['NUpload']
+    NUploadDragger: typeof import('naive-ui')['NUploadDragger']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    TheFooter: typeof import('./src/components/footer/TheFooter.vue')['default']
+    TheHeader: typeof import('./src/components/header/TheHeader.vue')['default']
+  }
+}
+//update by huber end
